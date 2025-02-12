1164b76d8380fc7d0d022320737f379436ae23b9
Alessandro Rodrigues <alessandjr0@gmail.com>
2025-02-11 22:44:31 -0300

    style: estizando cards

--- src/app/materiais/materiais/materiais.component.ts
+++ src/app/materiais/materiais/materiais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-materiais',
   standalone: false,
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class MateriaisComponent {
 
-}
+
+  }
